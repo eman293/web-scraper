1ab8fb298810a450f558fcaed5a3b38c81a7091d
eman293 <116614867+eman293@users.noreply.github.com>
2024-08-10 22:16:49 -0500

    done

--- This_Is_A_Folder/scraper-python.py
+++ This_Is_A_Folder/scraper-python.py
@@ -1,1 +1,17 @@
-print("hello world")
+#OPEN ENVIRONMENT WITH THE RUN BUTTON FIRST BEFORE DOING ANYTHING!!!!!!!!!!!!
+import requests as reqs
+from bs4 import BeautifulSoup
+
+def scrape():
+    
+    url = 'https://www.google.com/search?client=firefox-b-1-d&q=ddefinition+of+woke'
+    response = reqs.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print(soup.get_text())
+
+
+scrape()
+
+
+
+
